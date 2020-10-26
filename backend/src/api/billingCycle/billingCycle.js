b3b3069ca22be5312253a93abca6fc3b144afa41
unknown <maiaradepaulait@gmail.com>
2020-10-26 16:45:55 -0300

    Routes

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -1,23 +1,23 @@
-const restful = require ('node-restful')
-const { mongoose } = restful.mongoose
+const restful = require('node-restful')
+const mongoose = restful.mongoose
 
-const creditoSchema =   new mongoose.Schema({
-    name: { type: String, require: true},
-    value: { type: Number, require: true}
+const creditSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true }
 })
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, require: true},
-    value: { type: Number, require: true},
-    status: { type: String, require: false, uppercase: true,
-        enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
+    status: { type: String, required: false, uppercase: true,
+        enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
-const billingCycleSchema = new mongoose.Schema ({
-    name: { type: String, require: true},
-    month: {type: Number, min:1, max:12, require:true},
-    year: { type: Number, min: 1970, max: 2100, require: true},
-    credits: [creditoSchema],
+const billingCycleSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
+    year: { type: Number, min: 1970, max: 2100, required: true },
+    credits: [creditSchema],
     debts: [debtSchema]
 })
 
