1ae2be8d13122f8a34ab48e2574935ef6793ae85
unknown <maiaradepaulait@gmail.com>
2020-10-28 16:25:50 -0300

    Layout GRID for content using boodtrasp

---
+++ frontend/src/common/template/ContentHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <section className='content-header'>
+        <h1>{props.title} <smal>{props.smal}</smal></h1>
+    </section>
+)
--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ContentHeader from '../common/template/ContentHeader'
+import Content from '../common/template/Content'
+import ValueBox from '../common/widget/ValueBox'
 
-export default props => (
-    <div>
-        Dashboard
-    </div>
-)
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashbboard' smal='Versão 1.0' />
+                <Content >
+                    <ValueBox cols='12 4' color='green' icon='bank'
+                        value='R$ 10' text='Total de Créditos' />
+                   <ValueBox cols='12 4' color='red' icon='credit-card'
+                        value='R$ 10' text='Total de Débitos' />
+                    <ValueBox cols='12 4' color='blue' icon='money'
+                        value='R$ 10' text='Valor Consolidado' />
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
