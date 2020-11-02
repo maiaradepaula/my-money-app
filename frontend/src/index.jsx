61eebb668ef6e420727b6a86ca480d8cde087992
unknown <maiaradepaulait@gmail.com>
2020-11-02 11:05:37 -0300

    Doing 2 options of Dashboard - whit and out redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './main/App'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
 
-ReactDom.render(<App />, document.getElementById('app'))
+import promisse from 'redux-promise'
+
+import App from './main/App'
+import reducers from './main/reducers'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promisse)(createStore)(reducers, devTools)
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 import BillingCycle from '../billingCycle/BillingCycle'
-import Dashboard from '../dashboard/Dashboard'
+import Dashboard2 from '../dashboard2/Dashboard2'
 
 export default props => (
     <Router history={hashHistory} >
-        <Route path='/' component={Dashboard}/>
+        <Route path='/' component={Dashboard2}/>
         <Route path='/billingCycles' component={BillingCycle}/>
         <Redirect from='*' to='/' /> 
     </Router>
